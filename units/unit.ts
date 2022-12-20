c8b7de5ce7aa4fa24488a7aa62ae881d777dfdfb
Tarang74 <31427635+Tarang74@users.noreply.github.com>
2022-12-20 12:55:07 +1000

    Use nested hashmap instead of list

--- units/unit.ts
+++ units/unit.ts
@@ -1,15 +1,16 @@
 export default interface unit {
-    code: string;
-    title: string;
-    location: string;
-    attendanceModes: string[];
-    creditPoints: number;
-    prerequisites: string;
-    antirequisites: string;
-    equivalents: string;
-    coordinatorName: string;
-    coordinatorEmail: string;
-    CSPStudentContribution: number;
-    domesticTuition: number;
-    internationalTuition: number;
+    code: {
+        title: string;
+        location: string;
+        attendanceModes: string[];
+        creditPoints: number;
+        prerequisites: string;
+        antirequisites: string;
+        equivalents: string;
+        coordinatorName: string;
+        coordinatorEmail: string;
+        CSPStudentContribution: number;
+        domesticTuition: number;
+        internationalTuition: number;
+    }
 };
